b9e2d722903101b7a6f1408c93a5496ff51fa05a
Sudhan <sudhan@paradigmfms.com>
2025-11-24 18:02:47 +0530

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,51 +5,51 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: '.',
+  base: './',
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
-      manifestFilename: 'manifest.json',
-      devOptions: {
-        enabled: true
-      },
-      manifest: {
-        name: 'Paradigm IFS',
-        short_name: 'Paradigm',
-        description: 'Paradigm Integrated Field Services Application',
-        theme_color: '#007bff',
-        background_color: '#ffffff',
-        display: 'standalone',
-        orientation: 'portrait',
-        start_url: '/',
-        icons: [
-          {
-            src: '/Paradigm-Logo-3-1024x157.png',
-            sizes: '1024x157',
-            type: 'image/png'
-          },
-          {
-            src: '/Paradigm-Logo-3-1024x157.png',
-            sizes: '192x192',
-            type: 'image/png',
-            purpose: 'any maskable'
-          },
-          {
-            src: '/Paradigm-Logo-3-1024x157.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ],
-        protocol_handlers: [
-          {
-            protocol: 'web+paradigm',
-            url: '/handle-protocol?%s'
-          }
-        ]
-      }
-    })
+    // VitePWA({
+    //   registerType: 'autoUpdate',
+    //   includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+    //   manifestFilename: 'manifest.json',
+    //   devOptions: {
+    //     enabled: true
+    //   },
+    //   manifest: {
+    //     name: 'Paradigm IFS',
+    //     short_name: 'Paradigm',
+    //     description: 'Paradigm Integrated Field Services Application',
+    //     theme_color: '#007bff',
+    //     background_color: '#ffffff',
+    //     display: 'standalone',
+    //     orientation: 'portrait',
+    //     start_url: '/',
+    //     icons: [
+    //       {
+    //         src: '/Paradigm-Logo-3-1024x157.png',
+    //         sizes: '1024x157',
+    //         type: 'image/png'
+    //       },
+    //       {
+    //         src: '/Paradigm-Logo-3-1024x157.png',
+    //         sizes: '192x192',
+    //         type: 'image/png',
+    //         purpose: 'any maskable'
+    //       },
+    //       {
+    //         src: '/Paradigm-Logo-3-1024x157.png',
+    //         sizes: '512x512',
+    //         type: 'image/png'
+    //       }
+    //     ],
+    //     protocol_handlers: [
+    //       {
+    //         protocol: 'web+paradigm',
+    //         url: '/handle-protocol?%s'
+    //       }
+    //     ]
+    //   }
+    // })
   ],
 
 
